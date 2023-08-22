188fa0cfbd7109cd2803a54fdebe9a11df220048
Shahryar Ahmad <shehryarshahid49@gmail.com>
2023-08-22 10:51:04 +0500

    Add files via upload

---
+++ plutonium.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "apr_hash.h"
+#include "ap_config.h"
+#include "ap_provider.h"
+#include "httpd.h"
+#include "http_core.h"
+#include "http_config.h"
+#include "http_log.h"
+#include "http_protocol.h"
+#include "http_request.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+  void runCode(const char*);
+  
+#ifdef __cplusplus
+}
+#endif
